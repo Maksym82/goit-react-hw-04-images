6a9f4f5c83099c2011af26486a6d72189c86ed83
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-08-25 16:37:58 +0300

    update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import fetchImages from '../../services/api';
 import { Container } from './App.styled';
 import Searchbar from '../Searchbar/Searchbar';
@@ -9,79 +9,49 @@ import Modal from '../Modal/Modal';
 import Notiflix from 'notiflix';
 
 
-export  class App extends Component {
-  state = {
-    images: [],
-    page: 1,
-    query: '',
-    totalPages: 0,
-    isLoading: false,
-    showModal: false,
-    largeImageUrl: '',
+export default function App() {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [totalPages, setTotalPages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [largeImage, setLargeImage] = useState('');
+
+
+  const openModal = id => {
+    setIsLoading(true);
+    const largeImage = images.find(image => image.id === id);
+
+    setTimeout(() => {
+      setLargeImage(largeImage.largeImageURL);
+      setIsLoading(false);
+      setShowModal(true);
+    }, 500);
+    window.addEventListener('keydown', handleKeyDown);
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      this.state.page !== prevState.page ||
-      this.state.query !== prevState.query
-    ) {
-      this.fetchImageToArray(prevState);
+  
+
+  const deleteEventList = () => {
+    setShowModal(false);
+    window.removeEventListener('keydown', handleKeyDown);
+  };
+
+  const handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      deleteEventList;
+    }
+  };
+
+  const onClickBackdrop = event => {
+    if (event.currentTarget === event.target) {
+      deleteEventList;
     }
   }
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-  };
 
-  openModal = id => {
-    this.setState({ isLoading: true });
-    const largeImage = this.state.images.find(image => image.id === id);
 
-    setTimeout(() => {
-      this.setState({
-        largeImageURL: largeImage.largeImageURL,
-        isLoading: false,
-      });
-      this.toggleModal();
-    }, 500);
-  };
-
-  handleSubmit = query => {
-    this.setState({ images: [], query: query, page: 1 });
-    if (!query) {
-      Notiflix.Notify.warning('Enter your request!');
-    }
-  };
-
-  buttonOnClick = () => {
-    this.setState({
-      page: this.state.page + 1,
-    });
-  };
-
-  fetchImageToArray = async prevState => {
-    try {
-      this.setState({ isLoading: true });
-      const { query, page } = this.state;
-      const { totalHits, hits } = await fetchImages(query, page);
-      const pageCount = totalHits / 12;
-      this.setState({
-        totalPages: pageCount,
-      });
-      setTimeout(() => {
-        if (page !== prevState.page) {
-          this.setState(prevState => ({
-            images: [...prevState.images, ...hits],
-            isLoading: false,
-          }));
-        } else {
-          this.setState({ images: hits, isLoading: false });
-        }
-      }, 1000);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   render() {
     const {
